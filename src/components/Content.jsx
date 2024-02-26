dce9959e8057df7ebfa3b136ad9418bdb4bb98a3
Ghruank <ghruankkothare@gmail.com>
2024-02-26 23:29:43 +0530

    title

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,8 +7,7 @@ const Content=()=>{
             <div className="block" id="about">
                 <div className="header">About Me</div>
                 <div className="maincon">
-                <p>Ghruank Kothare<br/><br/>Pursuing BTech in Information Technology<br/>at Veermata Jijabai Technological Institute, Mumbai<br/><br/>Web and Software Developer and a fond DSA enjoyer<br/>Machine Learning enthusiast</p>
-                <img className="image" src={'/foto.jpg'} alt="" />
+                <p className="conc">Ghruank Kothare<br/><br/>Pursuing BTech in Information Technology<br/>at Veermata Jijabai Technological Institute, Mumbai<br/><br/>Web and Software Developer and a fond DSA enjoyer<br/>Machine Learning enthusiast</p>
                 </div>
             </div>
             <div id="skillsd">
